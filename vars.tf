d10785be3ce4ee89b64c75544587e6d2ced5337a
cyb3rmxrts <148910599+cyb3rmxrts@users.noreply.github.com>
2024-01-28 13:56:43 -0500

    infrastructure updates

--- vars.tf
+++ vars.tf
@@ -7,16 +7,16 @@ variable "default_tags" {
 }
 
 variable "sg_db_ingress" {
-  
+
 }
 
 variable "sg_db_egress" {
-  
+
 }
 
 variable "db_credentials" {
-  type = map(any)
-  sensitive = true 
+  type      = map(any)
+  sensitive = true
   default = {
     username = "username"
     password = "password"
