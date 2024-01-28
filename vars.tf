5502092b209facac7936ca03fc1dfa1eb48377a6
cyb3rmxrts <148910599+cyb3rmxrts@users.noreply.github.com>
2024-01-28 13:37:24 -0500

    vars.tf update

---
+++ vars.tf
@@ -0,0 +1,24 @@
+variable "default_tags" {
+  type = map(string)
+  default = {
+    "env" = "thundercats"
+  }
+  description = "Description for thundercats variable"
+}
+
+variable "sg_db_ingress" {
+  
+}
+
+variable "sg_db_egress" {
+  
+}
+
+variable "db_credentials" {
+  type = map(any)
+  sensitive = true 
+  default = {
+    username = "username"
+    password = "password"
+  }
+}
